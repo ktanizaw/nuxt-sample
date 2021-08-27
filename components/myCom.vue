8c9ad7347b9b0d8a3e14b0deee2a70a03a41f7c9
Kenta Tanizawa <kt10025102@gmail.com>
2021-08-27 23:06:53 +0900

    Nuxtの練習

---
+++ components/myCom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mycom">
+    <p>名前: <slot name="name">tanizawakenta</slot></p>
+    <p>住所: <slot name="address">kamata</slot></p>
+    <p>name:<slot :userNm="userNm">{{ userNm.enName }}</slot></p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyCom',
+  data () {
+    return {
+      userNm: {
+        enName: 'Tanizawa Kenta',
+        jpName: '谷澤健太' // ←slot内で参照したいデータ
+      }
+    }
+  }
+}
+</script>
+<style>
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -7,15 +7,40 @@
   <div>
     <label for="test">
   <input type="checkbox"><option value="1"></option>
-  
+
   </label>
   </div>
   <Button/>
+  <div class="home">
+    <MyCom>
+      <template #name>
+      谷澤健太
+      </template>
+    </MyCom>
+    <MyCom>
+      <template #address>
+      蒲田
+      </template>
+    </MyCom>
+    <MyCom v-slot:default="slotProps">
+      {{ slotProps.userNm.jpName }}
+    </MyCom>
+    <MyCom #default="user">
+      <div class="item">
+        {{ user.userNm }}
+      </div>
+    </MyCom>
+  </div>
 </div>
 </template>
 
 <script lang="ts">
+import MyCom from '../../components/myCom.vue'
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    MyCom
+  }
+})
 </script>
